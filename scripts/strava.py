876f78c03237e01f457fa0360a0dc1ddf93e0dfa
laurasantamaria-dotcom <laurasantamaria@gmail.com>
2026-07-22 14:35:28 +0200

    Actualizar strava.py

--- scripts/strava.py
+++ scripts/strava.py
@@ -5,21 +5,33 @@ import requests
 CLIENT_ID = os.environ["STRAVA_CLIENT_ID"]
 CLIENT_SECRET = os.environ["STRAVA_CLIENT_SECRET"]
 REFRESH_TOKEN = os.environ["STRAVA_REFRESH_TOKEN"]
+CODE = os.environ.get("STRAVA_CODE")
 
 print("CLIENT_ID =", CLIENT_ID)
 print("CLIENT_SECRET longitud =", len(CLIENT_SECRET))
 print("REFRESH_TOKEN longitud =", len(REFRESH_TOKEN))
 
-# Renovar l'access token
-token = requests.post(
-    "https://www.strava.com/oauth/token",
-    data={
-        "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET,
-        "grant_type": "refresh_token",
-        "refresh_token": REFRESH_TOKEN,
-    },
-)
+# Obtener access token
+if CODE:
+    token = requests.post(
+        "https://www.strava.com/oauth/token",
+        data={
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "code": CODE,
+            "grant_type": "authorization_code",
+        },
+    )
+else:
+    token = requests.post(
+        "https://www.strava.com/oauth/token",
+        data={
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "grant_type": "refresh_token",
+            "refresh_token": REFRESH_TOKEN,
+        },
+    )
 
 if not token.ok:
     print(token.text)
@@ -27,12 +39,15 @@ if not token.ok:
 
 token_data = token.json()
 
+print("NOU REFRESH TOKEN:")
+print(token_data["refresh_token"])
+
 access_token = token_data["access_token"]
 
 headers = {
     "Authorization": f"Bearer {access_token}"
 }
-# Descarregar les últimes activitats
+
 response = requests.get(
     "https://www.strava.com/api/v3/athlete/activities?per_page=20",
     headers=headers,
@@ -45,7 +60,6 @@ activities = response.json()
 entrenaments = []
 
 for a in activities:
-
     entrenaments.append({
         "id": a["id"],
         "data": a["start_date_local"][:10],
@@ -53,16 +67,15 @@ for a in activities:
         "tipus": a["sport_type"],
         "distancia": round(a["distance"] / 1000, 2),
         "desnivell": round(a["total_elevation_gain"]),
-        "temps": round(a["moving_time"] / 60)
-   })
-# Guardar entrenaments.json
+        "temps": round(a["moving_time"] / 60),
+    })
 
 with open("entrenaments.json", "w", encoding="utf-8") as f:
     json.dump(
         entrenaments,
         f,
         ensure_ascii=False,
-        indent=2
+        indent=2,
     )
 
 print(f"{len(entrenaments)} entrenaments desats a entrenaments.json")
